311c923150fee5a3dc113233f887a58690f358a2
Kevin Sheng <kevinsheng2005@gmail.com>
2025-04-17 16:55:34 -0700

    uh some general skeleton for vit 2

--- shell/shell.c
+++ shell/shell.c
@@ -1,6 +1,7 @@
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <linux/limits.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -31,6 +32,8 @@ pid_t shell_pgid;
 
 int cmd_exit(struct tokens *tokens);
 int cmd_help(struct tokens *tokens);
+int cmd_cd(struct tokens *tokens);
+int cmd_pwd(struct tokens *tokens);
 
 /* Built-in command functions take token array (see parse.h) and return int */
 typedef int cmd_fun_t(struct tokens *tokens);
@@ -45,6 +48,8 @@ typedef struct fun_desc {
 fun_desc_t cmd_table[] = {
     {cmd_help, "?", "show this help menu"},
     {cmd_exit, "exit", "exit the command shell"},
+    {cmd_cd, "cd", "change the current working directory"},
+    {cmd_pwd, "pwd", "print the current working directory"},
 };
 
 /* Prints a helpful description for the given command */
@@ -56,20 +61,67 @@ int cmd_help(unused struct tokens *tokens) {
 }
 
 /* Exits this shell */
-int cmd_exit(unused struct tokens *tokens) {
-    exit(0);
+int cmd_exit(unused struct tokens *tokens) { exit(0); }
+
+int cmd_cd(struct tokens *tokens) {
+    if (tokens_get_length(tokens) != 2) {
+        printf("cd only takes 1 arg\n");
+        return 1;
+    }
+    if (chdir(tokens_get_token(tokens, 1)) != 0) {
+        perror("chdir() error");
+        return 1;
+    }
+    return 0;
+}
+
+int cmd_pwd(unused struct tokens *tokens) {
+    char path[PATH_MAX];
+    if (getcwd(path, sizeof(path)) != NULL) {
+        printf("%s\n", path);
+    } else {
+        perror("getcwd() error");
+        return 1;
+    }
+    return 0;
 }
 
 /* Looks up the built-in command, if it exists. */
 int lookup(char *cmd) {
     if (cmd != NULL) {
         for (int i = 0; i < sizeof(cmd_table) / sizeof(fun_desc_t); i++) {
-            if (strcmp(cmd_table[i].cmd, cmd) == 0) {
-                return i;
-            }
+            if (strcmp(cmd_table[i].cmd, cmd) == 0) { return i; }
         }
     }
     return -1;
+}
+
+const char *resolve_cmd(const char *cmd) {
+    const char *PATH = getenv("PATH");
+    const char *cmd_copy = strdup(cmd);
+
+    if (PATH == NULL) { return cmd_copy; }
+
+    const int len = strlen(PATH);
+    int start = 0;
+    for (int i = 0; i < len + 1; i++) {
+        if (i == len || PATH[i] == ':') {
+            const int dir_len = i - start;
+            char *total_path = malloc(dir_len + len + 2);
+            strncpy(total_path, PATH + start, dir_len);
+            strcpy(total_path + dir_len + 1, cmd);
+            total_path[dir_len] = '/';
+            int test = open(total_path, O_RDONLY);
+            if (test != -1) {
+                free((char *)cmd_copy);
+                close(test);
+                return total_path;
+            }
+            free(total_path);
+            start = i + 1;
+        }
+    }
+    return cmd_copy;
 }
 
 /* Intialization procedures for this shell */
@@ -108,10 +160,10 @@ int main(unused int argc, unused char *argv[]) {
     int line_num = 0;
 
     /* Only print shell prompts when standard input is not a tty */
-    if (shell_is_interactive) {
-        fprintf(stdout, "%d: ", line_num);
-    }
+    if (shell_is_interactive) { fprintf(stdout, "%d: ", line_num); }
 
+    signal(SIGTTOU, SIG_IGN);
+    signal(SIGTTIN, SIG_IGN);
     while (fgets(line, 4096, stdin)) {
         /* Split our line into words. */
         struct tokens *tokens = tokenize(line);
@@ -122,8 +174,72 @@ int main(unused int argc, unused char *argv[]) {
         if (fundex >= 0) {
             cmd_table[fundex].fun(tokens);
         } else {
-            /* REPLACE this to run commands as programs. */
-            fprintf(stdout, "This shell doesn't know how to run programs.\n");
+            const char *prog = resolve_cmd(tokens_get_token(tokens, 0));
+
+            pid_t kid;
+            if ((kid = fork()) < 0) {
+                perror("fork");
+            } else if (kid == 0) {
+                if (setpgrp() != 0) {
+                    perror("setpgrp");
+                    exit(1);
+                }
+                if (tcsetpgrp(fileno(stdin), getpgrp()) != 0) {
+                    perror("tcsetpgrp");
+                    exit(1);
+                }
+                signal(SIGTTOU, SIG_DFL);
+                signal(SIGTTIN, SIG_DFL);
+
+                size_t len = tokens_get_length(tokens);
+                char **args = malloc((len + 1) * sizeof(char *));
+
+                size_t pos = 0, args_at = 0;
+                char *fin = NULL, *fout = NULL;
+                while (pos < len) {
+                    char *tok = tokens_get_token(tokens, pos);
+                    if (strcmp(tok, "<") == 0) {
+                        if (pos == len - 1) {
+                            fprintf(stderr, "Syntax error: Expected filename after <");
+                            exit(1);
+                        }
+                        fin = tokens_get_token(tokens, pos + 1);
+                        pos++;
+                    } else if (strcmp(tok, ">") == 0) {
+                        if (pos == len - 1) {
+                            fprintf(stderr, "Syntax error: Expected filename after >");
+                            exit(1);
+                        }
+                        fout = tokens_get_token(tokens, pos + 1);
+                        pos++;
+                    } else {
+                        args[args_at++] = tok;
+                    }
+                    pos++;
+                }
+                args[args_at] = NULL;
+
+                if (fin != NULL) {
+                    int in_fd = open(fin, O_RDONLY);
+                    if (in_fd < 0) { perror("open"); }
+                    dup2(in_fd, fileno(stdin));
+                }
+                if (fout != NULL) {
+                    int out_fd = open(fout, O_WRONLY | O_CREAT, 0666);
+                    if (out_fd < 0) { perror("open"); }
+                    dup2(out_fd, fileno(stdout));
+                }
+
+                if (execv(prog, args) != 0) {
+                    perror("execv");
+                    exit(1);
+                }
+            } else {
+                waitpid(kid, NULL, 0);
+                tcsetpgrp(0, getpgrp());
+            }
+
+            free((char *)prog);
         }
 
         if (shell_is_interactive) {
@@ -134,6 +250,4 @@ int main(unused int argc, unused char *argv[]) {
         /* Clean up memory. */
         tokens_destroy(tokens);
     }
-
-    return 0;
 }
